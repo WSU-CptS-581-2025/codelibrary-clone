0fbc4c83ec6c7a6b68768aad73ff60315f34ff54
fruitslayer1 <47836668+fruitslayer1@users.noreply.github.com>
2025-03-02 17:00:00 -0800

    Revert "expanded Fraction class"
    This reverts commit 3239f09bf59ee423fecb7dd96004b67fe6c47fe6.

--- java/numeric/Fraction.java
+++ java/numeric/Fraction.java
@@ -1,10 +1,10 @@
 package numeric;
 
 public class Fraction implements Comparable<Fraction> {
-    public long a, b; 
+    public long a, b;
 
     public Fraction(long a, long b) {
-        if (b < 0) { 
+        if (b < 0) {
             a = -a;
             b = -b;
         }
@@ -39,91 +39,13 @@ public class Fraction implements Comparable<Fraction> {
 
     @Override
     public int compareTo(Fraction o) {
-    	return Long.compare(this.a/this.b, o.a/o.b);
-    }
-    
-    public boolean lessThan(Fraction o) {
-    	if (this.compareTo(o) < 0)
-    		return true;
-    	return false;
-    }
-    
-    public boolean greaterThan(Fraction o) {
-    	if (this.compareTo(o) > 0)
-    		return true;
-    	return false;
-    }
-    
-    public boolean equals(Fraction o) {
-    	if (this.compareTo(o) == 0)
-    		return true;
-    	return false;
-    }
-    
-    public boolean notEqual(Fraction o) {
-    	return !(this.equals(o));
-    }
-    
-    public boolean lessThanOrEqual(Fraction o) {
-    	if (this.compareTo(o) <= 0)
-    		return true;
-    	return false;
-    }
-    
-    public boolean greaterThanOrEqual(Fraction o) {
-    	if (this.compareTo(o) >= 0)
-    		return true;
-    	return false;
+        return Long.compare(this.a * o.b, this.b * o.a);
     }
 
     @Override
     public String toString() {
         return a + "/" + b;
     }
-    
-    public Fraction mediant(Fraction f, Fraction g, long wf, long wg) {
-    	return new Fraction(f.a*wf + g.a*wg, f.b*wf + g.b*wg);
-    }
-    
-    public Fraction mediant(Fraction f, Fraction g) {
-    	return new Fraction(f.a + g.a, f.b + g.b);
-    }
-    
-    // find smallest fraction between two fractions using Stern-Brocot tree
-    public Fraction findSmallestBetween(Fraction o) { 
-    	Fraction l = new Fraction(0,1);
-    	Fraction r = new Fraction(1,0);
-    	while (true) {
-    		Fraction m = mediant(l,r);
-    		long bl = 0, br = 1;
-    		if (m.lessThanOrEqual(this)) {
-    			while (mediant(l,r,1,br).lessThanOrEqual(this))
-    				br *= 2;
-    			while (bl + 1 != br) {
-    				long bm = (bl + br) / 2;
-    				if (mediant(l,r,1,bm).lessThanOrEqual(this))
-    					bl=bm;
-    				else
-    					br=bm;
-    			}
-    			l = mediant(l,r,1,bl);
-    		} else if (m.greaterThanOrEqual(o)) {
-    			while (mediant(l,r,br,1).greaterThanOrEqual(o))
-    				br *= 2;
-    			while (bl + 1 != br) {
-    				long bm = (bl+br) / 2;
-    				if (mediant(l,r,bm,1).greaterThanOrEqual(o))
-    					bl = bm;
-    				else
-    					br=bm;
-    			}
-    			r = mediant(l,r,bl,1);
-    		} else {
-    			return m;
-    		}	
-    	}
-    }
-    
 
     // Usage example
     public static void main(String[] args) {
